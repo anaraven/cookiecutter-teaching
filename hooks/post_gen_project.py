a5c867968431afc4554843da42f5a55d2e7e8aae
Andres Aravena <andres.aravena@gmail.com>
2024-01-08 19:28:42 +0300

    make sure of deleting all `.keep` files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,6 +10,14 @@ def remove_file(filepath):
 
 if __name__ == '__main__':
     
-    remove_file('results/.keep')
+    remove_file('ans/.keep')
+    remove_file('bib/.keep')
+    remove_file('buro/.keep')
+    remove_file('code/.keep')
     remove_file('data/.keep')
+    remove_file('docs/.keep')
+    remove_file('eval/.keep')
+    remove_file('extra/.keep')
+    remove_file('results/.keep')
+    remove_file('slides/.keep')
 
